11f8e6dca6a23d39cf3f8b8028deed574b608b48
shizumenfa <58022797+shizumenfa@users.noreply.github.com>
2020-09-22 20:56:39 +0800

    master

--- js.js
+++ js.js
@@ -1,261 +1,238 @@
-// 封装数学对象
-var myMath = {
-    PI: 3.1415926,
-    max: 　 function() {
-        var max = arguments[0];
-        for (var i = 0; i < arguments.length; i++) {
-            if (arguments[i] > max) {
-                max = arguments[i];
-            }
+/* var age = 20;
+console.log(isNaN(age));
+var usname = 'suchao';
+console.log(isNaN(usname));
 
-        }
-        return max;
-    },
-    min: 　 function() {
-        var min = arguments[0];
-        for (var i = 0; i < arguments.length; i++) {
-            if (arguments[i] < min) {
-                min = arguments[i];
-            }
+console.log(age.toString());
+console.log(String(age));
+console.log(age + '');
 
-        }
-        return min;
+var num = '201';
+console.log(parseInt(num));
+console.log(parseFloat(num));
+console.log(Number(num));
+console.log(num - 0);
+
+var str = 0;
+console.log(Boolean(str));
+str = 1;
+console.log(Boolean(str));
+str = undefined;
+console.log(Boolean(str));
+str = null;
+console.log(Boolean(str));
+
+console.log(5 > 3 && 3 < 5);
+console.log(5 < 3 && 5 > 3);
+console.log(5 > 3 || 3 < 5);
+console.log(5 < 3 || 5 > 3);
+var and = 5 > 3;
+console.log(and);
+console.log(!and);
+
+console.log(123 && 456);
+console.log(123 || 456);
+
+str = str + 1;
+console.log(str);
+str += 1;
+console.log(str);
+
+var popUp = prompt('输入成绩')
+if (popUp == 100) {
+    alert('你真np');
+} else if (popUp >= 90) {
+    alert('你真不错');
+} else if (popUp >= 60) {
+    alert('有点意思');
+}
+
+var usname = prompt('请输入姓名')
+switch (usname) {
+    case '苏超':
+        console.log('你的成绩是100');
+        break;
+    case '孟凡琦':
+        console.log('你的成绩是垃圾');
+        break;
+    default:
+        console.log('名字你都输入不对啊');
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log('我爱说垃圾话');
+
+}
+
+var str = '';
+for (var i = 1; i <= 5; i++) {
+
+    for (let j = 1; j <= 5; j++) {
+        str = str + '★';
     }
-
+    str += '\n';
 }
-console.log(myMath.max(1, 2, 3, 4, 5));
-console.log(myMath.min(1, 2, 3, 4, 0.1));
-console.log(myMath.PI);
+console.log(str);
 
+var han = prompt('han');
+var lie = prompt('lie');
+var str = '';
+for (var i = 1; i <= han; i++) {
+    for (let k = 1; k < lie; k++) {
+        str += '★';
 
-// Math绝对值
-console.log(Math.abs(-20));
-console.log(Math.abs('20'));
-console.log(Math.abs('suchao'));
-//20
-//20
-//NaN
-
-//取整
-//向上
-console.log(Math.floor(1.2));
-console.log(Math.floor(1.9));
-//1
-//1
-
-// 向下
-console.log(Math.ceil(2.3));
-//3
-
-// 四舍五入
-console.log(Math.round(1.3));
-console.log(Math.round(1.5));
-console.log(Math.round(1.6));
-//1
-//2
-//2
-// 特殊 .5 往大了取
-console.log(Math.round(-1.5));
-//-1
-
-
-
-// console.log(Math.random());
-//随机整数
-function get(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min; //含最大值，含最小值 
+    }
+    str += '\n';
 }
-console.log(get(1, 10));
+console.log(str);
 
-//随机点名
-var arr = ['一', '二', '三', '四', '五']
-console.log(arr[get(0, arr.length - 1)]);
-
-function get(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min; //含最大值，含最小值 
+var i = 1;
+while (i <= 10) {
+    console.log('你妈死了');
+    i++;
 }
-var random = get(1, 10);
-while (true) {
-    var num = prompt('请输入1~10进行游戏')
-    if (num > random) {
-        alert('猜大了');
-    } else if (num < random) {
-        alert('猜小了');
-    } else {
-        alert('猜对了');
+
+do {
+    console.log('尼玛死了' + i + '次');
+    i++;
+} while (i <= 20);
+
+for (var i = 0; i <= 10; i++) {
+    if (i == 4) {
+        continue;
+    }
+    console.log('你妈死了' + i + '次');
+}
+
+for (var i = 0; i <= 10; i++) {
+    if (i == 4) {
         break;
     }
+    console.log('你妈死了' + i + '次');
+}
 
+var arr = [1, 2, 3, 4, 5];
+console.log(arr.length);
+console.log(arr[0]);
+for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+
+}
+arr.length = 6;
+console.log(arr.length);
+arr[5] = 6;
+console.log(arr[5]);
+arr[4] = 7;
+console.log(arr[4]);
+
+function star(han, lie) {
+    var str = '';
+    for (var i = 1; i <= han; i++) {
+        for (var j = 1; j <= lie; j++) {
+            str += '★';
+
+        }
+        str += '\n';
+    }
+    return str;
+}
+var h = prompt('hang');
+var l = prompt('lie');
+parseInt(h);
+parseInt(l);
+console.log(star(h, l));
+
+
+function fn1() {
+    console.log(111);
+    fn2();
+    console.log('fn1');
+}
+
+function fn2() {
+    console.log(222);
+    console.log('fn2');
+}
+fn1();
+
+var obj = {
+    name: 'suchao',
+    sex: 'nan'
+}
+for (var k in obj) {
+    // console.log(k);
+    console.log(obj[k]);
 }
 
 
-// 获取日期时间
-var date = new Date();
-console.log(date);
-var date1 = new Date('2019-10-1 8:8:8')
-console.log(date1);
+//字面量
+var obj = {
+    usname: 'suchao',
+    sex: 'nan',
+    age: '20',
+    sayHi: function() {
+        console.log('我是你的爸爸');
+    }
+}
+console.log(obj.usname);
+console.log(obj.sex);
+console.log(obj.age);
+obj.sayHi();
 
-var date = new Date();
+//new Object
+var obj = new Object();
+obj.name = 'suchao';
+obj.age = '20';
+obj.sex = 'nan';
+obj.sayHi = function() {
+    console.log('我还是你的爸爸');
+}
+console.log(obj.name);
+console.log(obj.sex);
+console.log(obj.age);
+obj.sayHi();
+
+
+//构造函数
+function obj(name, age, sex) {
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+    this.sing = function(sang) {
+        console.log(sang);
+    }
+}
+var suchao = new obj('苏超', '20', '男');
+console.log(suchao);
+suchao.sing('动漫歌曲');
+
+var menfanqi = new obj('孟凡琦', '15', '男');
+console.log(menfanqi);
+menfanqi.sing('我是大笨蛋');
+
+console.log(suchao.name);
+for (var k in suchao) {
+    console.log(k);
+}
+for (var i in menfanqi) {
+    console.log(suchao[i]);
+}
+
+
+var num = 3.4;
+console.log(Math.floor(num));
+console.log(Math.ceil(num));
+console.log(Math.round(num));
+var abs = -3;
+console.log(Math.abs(abs));
+*/
+
+var date = new Date('2023/5/23');
+console.log(date);
 console.log(date.getFullYear());
-console.log(date.getMonth() + 1);
+console.log(date.getMonth());
 console.log(date.getDate());
 console.log(date.getDay());
-
-//获取年月日星期
-var year = date.getFullYear();
-var month = date.getMonth() + 1;
-var date1 = date.getDate();
-var arr = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
-var week = date.getDay();
-var time = year + '年' + 　month + '月' + date1 + '日 ' + arr[week];
-console.log(time);
-
-//获取时分秒
-var hours = date.getHours();
-var min = date.getUTCMinutes();
-var sec = date.getSeconds();
-console.log(hours, min, sec);
-
-//获取现在的时间 年-月-日 星期 时：分：秒格式
-function times() {
-    var date = new Date();
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    var date1 = date.getDate();
-    var arr = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
-    var week = date.getDay();
-    var hours = date.getHours();
-    hours = hours < 10 ? '0' + hours : hours
-    var min = date.getUTCMinutes();
-    min = min < 10 ? '0' + min : min;
-    var sec = date.getSeconds();
-    sec = sec < 10 ? '0' + sec : sec
-    var time = year + '年' + 　month + '月' + date1 + '日 ' + arr[week] + ' ' + hours + ':' + min + ':' + sec;
-    return time;
-}
-console.log(times());
-
-//获得总毫秒数
-var time = new Date();
-console.log(time.valueOf());
-console.log(time.getTime());
-
-var time_1 = +new Date();
-console.log(time_1);
-
-console.log(Date.now());
-
-
-//倒计时
-function countdown(time) {
-    var nowTime = +new Date;
-    var inPut = +new Date(time);
-    var times = (inPut - nowTime) / 1000;
-    var d = parseInt(times / 60 / 60 / 24);
-    var h = parseInt(times / 60 / 60 % 24);
-    var m = parseInt(times / 60 % 60);
-    var s = parseInt(times % 60);
-    return d + '天' + h + '时' + m + '分' + s + '秒';
-
-}
-console.log(countdown('2020-9-21 8:8:8'));
-
-//instanceof运算符 判断是否是数组
-var arr = [];
-var obj = {};
-console.log(arr instanceof Array); //true
-console.log(obj instanceof Array); //false
-
-//Array.isArray
-console.log(Array.isArray(arr));
-
-
-
-//新增数组元素
-var arr = [1, 2, 3];
-// arr.push(4, 5);
-console.log(arr.push(4, 5));
-// arr.unshift('suchao');
-console.log(arr.unshift('suchao'));
-console.log(arr);
-
-//删除数组元素
-// arr.pop();
-console.log(arr.pop());
-console.log(arr);
-arr.shift();
-console.log(arr);
-
-
-
-//筛选数组
-var num = [1200, 1300, 2000, 2500];
-var newNum = [];
-for (var i = 0; i < num.length; i++) {
-    if (num[i] < 2000) {
-        // newNum[newNum.length] = num[i];
-        newNum.push(num[i])
-
-    }
-
-}
-console.log(newNum);
-
-
-
-//翻转数组
-var arr = [1200, 1300, 2000, 2500];
-arr.reverse();
-console.log(arr);
-
-//排序
-var arr = [3, 2, 1, 5, 4];
-arr.sort();
-console.log(arr);
-//两位数出现错误
-
-//[13, 2, 33, 4, 5]
-//解决方法
-//升序
-var arr1 = [33, 2, 13, 5, 4];
-arr1.sort(
-    function(a, b) {
-        return a - b;
-        // return b - a;//降序
-    }
-);
-console.log(arr1);
-// [2, 4, 5, 13, 33]
-
-
-
-//  获取数组元素索引
-var arr = [3, 2, 1, 5, 5, 4];
-// 从前向后查找
-console.log(arr.indexOf(5));
-// 从后向前查找
-console.log(arr.lastIndexOf(5));
-
-
-//数组去重
-var arr = ['c', 'a', 'z', 'a', 'x', 'a', 'x', 'c', 'b'];
-
-function unique(arr) {
-    var newArr = [];
-    for (var i = 0; i < arr.length; i++) {
-        if (newArr.indexOf(arr[i]) === -1) {
-            newArr.push(arr[i]);
-        }
-
-    }
-    return newArr;
-}
-var demo = unique(['c', 'a', 'z', 'a', 'x', 'a', 'x', 'c', 'b'])
-console.log(demo);
-
-
-//数组转换字符串
-// toString
-console.log(arr.toString());
-// join('分割符')
-console.log(arr.join('|'));
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
